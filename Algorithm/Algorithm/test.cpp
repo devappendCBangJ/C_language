ab0ee296cc6a249a302653a60b0f39dee833b994
devappendCBangJ <dev.appendCBangJ@gmail.com>
2022-01-14 20:28:59 +0900

    ver220114

--- Algorithm/Algorithm/test.cpp
+++ Algorithm/Algorithm/test.cpp
@@ -1,56 +1,52 @@
 #include <stdio.h>
 
+int reverse(int x) {
+	int tot = 0;
+	while (x != 0) {
+		tot += (x % 10);
+		tot *= 10; // rev * 10이 아니라 rev *= 10임. 실수 주의 ♣
+		x /= 10; // num[i] / 10가 아니라 num[i] /= 10임. 실수 주의 ♣
+		// printf("(%d)\n", num[i]); // 확인용 코드
+	}
+	tot /= 10; // rev / 10이 아니라 rev /= 10임. 실수 주의 ♣
+	return tot;
+}
+
+bool isPrime(int x) { // 하나의 함수에서 return을 여러개 하면 안된다 ♣
+	bool de_flag = 1;
+	if (x == 1) {
+		de_flag = 0; // 1은 소수가 아니다(약수가 자기 자신을 포함해서 2개가 되지 않으므로) ♣
+	}
+	for (int j = 2; j <= (x / 2); j++) {
+		if (x % j == 0) {
+			de_flag = 0;
+			break;
+		}
+	}
+	return de_flag;
+}
+
 int main() {
-// 변수
-	int total = 0;
-	int k = 0;
-	int i = 1;
-	int l = 1;
-	int food_times[100] = { 1 }; // 이렇게 해도 되나??? // 인덱스 0번은 쓸모 없으니까 아무값이나 넣음
-	int food_index[100];
-	int food_size;
-	bool food_count = 1;
-	int food_result;
-// 입력
-	scanf_s("%d", &k);
-	while (food_times[i-1] > 0) {
-		scanf_s("%d", &food_times[i]);
-		i++;
+	// 입력
+	int N;
+	scanf_s("%d", &N);
+	int num[101]; // 배열의 선언에서 인덱스는 변수로 설정할 수 없다. 상수로 설정해야한다 ♣
+	for (int i = 0; i < N; i++) {
+		scanf_s("%d", &num[i]);
 	}
-	food_times[i-1] = '\0';
-	// printf("%d\n", food_times[i]); // 확인용 코드
-// 연산
-	// 음식의 인덱스 기억
-	// food_size = sizeof(food_times) / sizeof(food_times[0]) - 1; // 이건 초기에 지정한 배열 크기가 대입됨
-	food_size = i-2; // 이렇게 해야됨
-	// printf("%d", food_size); // 확인용 코드
-	for(int j = 1; j <= food_size; j++){
-		food_index[j] = j;
-		// printf("%d\n", food_index[j]); // 확인용 코드
+
+	// 연산
+	int rev = 0;
+	bool deci_flag = 1;
+	for (int i = 0; i < N; i++) {
+		rev = reverse(num[i]);
+		// printf("(%d)\n", rev); // 확인용 코드
+		deci_flag = isPrime(rev);
+		// printf("(%d)\n", deci_flag); // 확인용 코드
+		if (deci_flag == 1) {
+			printf("%d ", rev); // rev 초기화 안하면 값 누적됨 ♣
+		}
+		deci_flag = 1;
+		rev = 0;
 	}
-	// 반복문 돌기
-	while (1) {
-		// printf("여깄다"); // 확인용 코드
-		// 현재 원소가 비어있지 않다면 현재 원소--, total++
-		if (food_count == 1 && food_times[l] != 0) {
-			food_times[l]--;
-			total++;
-		}
-		// 현재 원소가 비어있든 아니든 l++
-		l++;
-		// 반복문 회전 시키는 코드
-		if (l > food_size) {
-			l = 1;
-		}
-		// total과 l이 같아지면 멈춤
-		if (k == total) {
-			food_count == 0;
-			if (food_times[l] != 0) {
-				food_result = food_index[l];
-				break;
-			}
-		}
-	}
-	// 출력
-	printf("%d", food_result);
 }
