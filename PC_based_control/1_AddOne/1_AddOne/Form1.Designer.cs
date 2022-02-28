8037ce014fc1b42eedfec902033da76ba28dd729
devappendCBangJ <dev.appendCBangJ@gmail.com>
2022-03-01 08:23:03 +0900

    ver220301

---
+++ PC_based_control/1_AddOne/1_AddOne/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace _1_AddOne
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblNum = new System.Windows.Forms.Label();
+            this.Reset = new System.Windows.Forms.Button();
+            this.Add = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblNum
+            // 
+            this.lblNum.AutoSize = true;
+            this.lblNum.Font = new System.Drawing.Font("Verdana", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNum.ForeColor = System.Drawing.Color.Coral;
+            this.lblNum.Location = new System.Drawing.Point(26, 36);
+            this.lblNum.Name = "lblNum";
+            this.lblNum.Size = new System.Drawing.Size(70, 73);
+            this.lblNum.TabIndex = 0;
+            this.lblNum.Text = "0";
+            this.lblNum.Click += new System.EventHandler(this.lblNum_Click);
+            // 
+            // Reset
+            // 
+            this.Reset.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Reset.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Reset.Location = new System.Drawing.Point(338, 133);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(187, 102);
+            this.Reset.TabIndex = 1;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = false;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // Add
+            // 
+            this.Add.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Add.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Add.Location = new System.Drawing.Point(338, 251);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(187, 102);
+            this.Add.TabIndex = 2;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = false;
+            this.Add.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 380);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.lblNum);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNum;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ PC_based_control/1_AddOne/1_AddOne/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1_AddOne
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(lblNum.Text);
+            n++;
+            lblNum.Text = Convert.ToString(n);
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            lblNum.Text = "0";
+        }
+
+        private void lblNum_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
